5b3b6242decbb294cecb5fd02c1a9151156b2b9b
narendran-7 <narendran.dn97@gmail.com>
2023-05-21 00:49:38 +0530

    headless | fix DeprecationWarning:

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from os import path
 from selenium.webdriver.common.by import By 
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
 class Saavn:
     def __init__(self, url):
@@ -9,24 +11,35 @@ class Saavn:
 
         driver_bin = f'{get_path}/geckodriver.exe'
 
-        driver = webdriver.Firefox(executable_path=driver_bin)
+        firefox_options = Options()
+        firefox_options.add_argument('-headless')
+
+        driver_service = Service(driver_bin)
+
+        driver = webdriver.Firefox(service=driver_service, options=firefox_options)
 
         driver.get(url)
 
         driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/main/div[2]/figure/figcaption/div/p[1]/a').click()
         
-        logs = driver.get_log("browser")
+        # logs = driver.get_log('performance')
 
-        for tmp in logs:
-            print("------> ", tmp)
+        # print(logs)
+
+        # for tmp in logs:
+        #     print("------> ", tmp)
         print("okkkkkk")
+        print(driver.title)
+
+        # Quit the WebDriver
+        driver.quit()
 
 
 
 import httpx
 
 if __name__ == "__main__":
-    pass
-    #spark = Saavn("https://www.jiosaavn.com/song/raawadi/QCAGQjNiYHw")
+    # pass
+    spark = Saavn("https://www.jiosaavn.com/song/raawadi/QCAGQjNiYHw")
 
 # 
